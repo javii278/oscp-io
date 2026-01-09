3531e2bf166304eed660dc9047ac568ed89f8c84
javii278 <116780407+javii278@users.noreply.github.com>
2026-01-09 11:19:09 +0100

    Update layout.tsx

--- Clone-Credential-Verification/credential-clone/src/app/layout.tsx
+++ Clone-Credential-Verification/credential-clone/src/app/layout.tsx
@@ -31,23 +31,10 @@ export default function RootLayout({
           crossOrigin="anonymous"
           src="//unpkg.com/same-runtime/dist/index.global.js"
         />
-        {/* Favicon */}
-        <link rel="icon" type="image/jpeg" href="/accredible.jpeg" />
       </head>
       <body suppressHydrationWarning className="antialiased">
-        {/* Header con logo y título */}
-        <header className="flex items-center gap-2 p-4 bg-white shadow-md">
-          <img
-            src="/accredible.jpeg"
-            alt="OSCP Logo"
-            className="w-8 h-8 object-contain"
-          />
-          <h1 className="text-xl font-bold">Accredible - Certificates and Badges</h1>
-        </header>
-
         <ClientBody>{children}</ClientBody>
       </body>
     </html>
   );
 }
-
